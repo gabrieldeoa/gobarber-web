e2885f9f5258db2e6852d78c5572075372dbe40c
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-09-16 21:44:18 -0300

    using app provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,13 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/auth';
-import { ToastProvider } from './hooks/toast';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
-      <ToastProvider>
-        <SignIn />
-      </ToastProvider>
-    </AuthProvider>
-    <ToastContainer />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
     <GlobalStyle />
   </>
 );
