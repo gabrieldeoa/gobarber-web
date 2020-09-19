563ff24aa962de884d7222f2e81262cc03eb1907
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-09-19 11:34:10 -0300

    refactoring: create new component to cotent of toast

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { Container, Toast } from './styles';
+
+import Toast from './Toast';
+
+import { Container } from './styles';
 import { ToastMessage, useToast } from '../../hooks/toast';
 
 interface ToastContainerProps {
@@ -8,26 +10,10 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button onClick={() => removeToast(message.id)} type="button">
-            <FiXCircle size={20} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
